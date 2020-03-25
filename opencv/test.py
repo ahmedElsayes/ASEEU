45f496d627eaf04eaae7a46cee87a878a6ae145d
Ahmed Elsayes <mysterious.en2010@gmail.com>
2020-03-25 08:05:16 +0200

    commit

--- opencv/test.py
+++ opencv/test.py
@@ -1,9 +1,19 @@
 import numpy as np
 import cv2 as cv
+
 # Create a black image
-img = np.zeros((512, 512, 3), np.uint8)
+imgi = np.zeros((650, 1200, 3), np.uint8)
 # Draw a diagonal blue line with thickness of 5 px
-cv.rectangle(img, (384, 0), (510, 128), (0, 0, 255), -10)
-cv.imshow('test', img)
+
+start = 5
+end = 151
+cv.rectangle(img, (start, start), (end, end), (0, 255, 0), 2)
+# cv.imshow('test', img)
+for i in range(start,end,1):
+    cv.rectangle(img, (start, start), (i, end), (0, 0, 255), -1)
+    cv.imshow('test', img)
+    K = cv.waitKey(20)
+    if K == 27:
+        break
 cv.waitKey(0)
 cv.destroyAllWindows
